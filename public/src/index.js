6c9726d9648813cbf761288d2566ed4891880a2f
Anushka-Garg <34761959+Anushka-Garg@users.noreply.github.com>
2026-05-05 17:48:23 +0530

    Update index.js

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = document.getElementById("root");
+if (root) {
+  ReactDOM.createRoot(root).render(<App />);
+} else {
+  console.error("Root element not found");
+}
